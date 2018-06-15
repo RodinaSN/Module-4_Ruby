f62cd4e9e99149a7b4dc841d2648606686e4025d
RodinaSN <Rodina-ifz@ya.ru>
2018-06-15 17:14:07 +0300

    Ruby

---
+++ Number_days.rb
@@ -0,0 +1,7 @@
+Month = {
+  :January => "31",
+  :February => "28",
+  :March => "30"
+}
+
+Month[:January]
---
+++ Today_date.rb
@@ -0,0 +1,7 @@
+Today = {
+  :Day => "14",
+  :Month => "06",
+  :Year => "2018"
+}
+
+Today[:Day]
